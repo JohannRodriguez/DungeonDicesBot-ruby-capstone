a9d3e97554495f6577a73a6a7e86d668eace940b
Johann <themrblacksapce@hotmail.com>
2020-04-29 10:35:06 -0600

    replaced method arguments for attr_accesor in criticorblunder.rb

--- lib/criticorblunder.rb
+++ lib/criticorblunder.rb
@@ -4,70 +4,71 @@ class Criticblunder
   @@blunder = [[nil, nil, 4], [nil, nil, 6], [nil, nil, 8], [nil, nil, 8], [1, 1, 20], [95, 100, 100]]
   @@dice_index_array = [[4, 0], [6, 1], [8, 2], [10, 3], [20, 4], [100, 5]]
   @@valid_dice = [4, 6, 8, 10, 20, 100]
+
+  attr_accessor :assigner_1
+  attr_accessor :assigner_2
+  attr_accessor :assigner_text_1
+  attr_accessor :assigner_text_2
+
   def cr_bl_assign(dice_type, values, type_of_assign)
     return "Invalid assigner, try to use either 'cr' or 'bl'" unless type_of_assign == 'cr' or type_of_assign == 'bl'
 
     dice_type_index = dice_index(dice_type)
-    assigners = assigners_validation(type_of_assign)
-    assigner_1 = assigners[0]
-    assigner_2 = assigners[1]
-    assigner_text_1 = assigners[2]
-    assigner_text_2 = assigners[3]
-    return value_none(values, assigner_1, assigner_text_1, dice_type, dice_type_index) if values == 'none'
+    assigners_validation(type_of_assign)
+    return value_none(values, dice_type, dice_type_index) if values == 'none'
 
     new_values = values.split('to')
     return 'You can only assign a maximum of two values' if new_values.length > 2
 
-    return range_validation(assigner_2, assigner_1, assigner_text_2, assigner_text_1, new_values, dice_type_index) unless assigner_2[dice_type_index][0].nil?
+    return range_validation(new_values, dice_type_index) unless self.assigner_2[dice_type_index][0].nil?
 
-    set_values_for_assigner(assigner_1, new_values, dice_type_index)
-    return "Your #{assigner_text_1} roll must be within a range from 1 to #{assigner_1[dice_type_index][2]}" if assigner_1[dice_type_index][0].nil? or
+    set_values_for_assigner(new_values, dice_type_index)
+    return "Your #{self.assigner_text_1} roll must be within a range from 1 to #{self.assigner_1[dice_type_index][2]}" if self.assigner_1[dice_type_index][0].nil? or
 
-    return "The new value forD#{dice_type} #{assigner_text_1} is now #{assigner_1[dice_type_index][0]}" if assigner_1[dice_type_index][0] == assigner_1[dice_type_index][1]
-    "The new #{assigner_text_1} range for D#{dice_type} goes from #{assigner_1[dice_type_index][0]} to #{assigner_1[dice_type_index][1]}"
+    return "The new value for D#{dice_type} #{self.assigner_text_1} is now #{self.assigner_1[dice_type_index][0]}" if self.assigner_1[dice_type_index][0] == self.assigner_1[dice_type_index][1]
+    "The new #{self.assigner_text_1} range for D#{dice_type} goes from #{self.assigner_1[dice_type_index][0]} to #{self.assigner_1[dice_type_index][1]}"
   end
 
-  def range_validation(assigner_2, assigner_1, assigner_text_2, assigner_text_1, new_values, dice_type_index)
+  def range_validation(new_values, dice_type_index)
     new_values.length.times do |i|
-      if new_values[i].to_i.between?(assigner_2[dice_type_index][0], assigner_2[dice_type_index][1])
-        return "#{assigner_text_2.capitalize} value is #{assigner_2[dice_type_index][0]}. You can't assign #{new_values[i].to_i} to #{assigner_text_1}" if assigner_1[dice_type_index][0] == assigner_1[dice_type_index][1]
+      if new_values[i].to_i.between?(self.assigner_2[dice_type_index][0], self.assigner_2[dice_type_index][1])
+        return "#{self.assigner_text_2.capitalize} value is #{self.assigner_2[dice_type_index][0]}. You can't assign #{new_values[i].to_i} to #{self.assigner_text_1}" if self.assigner_1[dice_type_index][0] == self.assigner_1[dice_type_index][1]
 
-        return "#{assigner_text_2.capitalize} range goes from #{assigner_2[dice_type_index][0]} to #{assigner_2[dice_type_index][1]}. You can't assign #{new_values[i].to_i} to #{assigner_text_1}"
+        return "#{self.assigner_text_2.capitalize} range goes from #{self.assigner_2[dice_type_index][0]} to #{self.assigner_2[dice_type_index][1]}. You can't assign #{new_values[i].to_i} to #{self.assigner_text_1}"
       end
     end
   end
 
-  def set_values_for_assigner(as_1, new_values, dice_type_index)
-    as_1[dice_type_index][0] = new_values[0].to_i if new_values[0].to_i.between?(1, as_1[dice_type_index][2])
-    as_1[dice_type_index][1] = new_values[1].to_i if new_values.length > 1 and new_values[1].to_i.between?(1, as_1[dice_type_index][2])
-    as_1[dice_type_index][1] = as_1[dice_type_index][0] if new_values.length == 1
-    as_1[dice_type_index][1] = nil if as_1[dice_type_index][0].nil?
-    as_1[dice_type_index][0] = nil if as_1[dice_type_index][1].nil?
-    if as_1[dice_type_index][0].is_a?(Integer) and as_1[dice_type_index][1].is_a?(Integer)
-      as_1[dice_type_index][0], as_1[dice_type_index][1] = as_1[dice_type_index][1], as_1[dice_type_index][0] if as_1[dice_type_index][0] > as_1[dice_type_index][1]
+  def set_values_for_assigner(new_values, dice_type_index)
+    self.assigner_1[dice_type_index][0] = new_values[0].to_i if new_values[0].to_i.between?(1, self.assigner_1[dice_type_index][2])
+    self.assigner_1[dice_type_index][1] = new_values[1].to_i if new_values.length > 1 and new_values[1].to_i.between?(1, self.assigner_1[dice_type_index][2])
+    self.assigner_1[dice_type_index][1] = self.assigner_1[dice_type_index][0] if new_values.length == 1
+    self.assigner_1[dice_type_index][1] = nil if self.assigner_1[dice_type_index][0].nil?
+    self.assigner_1[dice_type_index][0] = nil if self.assigner_1[dice_type_index][1].nil?
+    if self.assigner_1[dice_type_index][0].is_a?(Integer) and self.assigner_1[dice_type_index][1].is_a?(Integer)
+      self.assigner_1[dice_type_index][0], self.assigner_1[dice_type_index][1] = self.assigner_1[dice_type_index][1], self.assigner_1[dice_type_index][0] if self.assigner_1[dice_type_index][0] > self.assigner_1[dice_type_index][1]
     end
   end
 
-  def value_none(value, as_1, as_1_text, dice_value, dtp)
-    as_1[dtp][0] = nil
-    as_1[dtp][1] = nil
-    "#{as_1_text.capitalize} D#{dice_value} value setted to none"
+  def value_none(value, dice_value, dtp)
+    self.assigner_1[dtp][0] = nil
+    self.assigner_1[dtp][1] = nil
+    "#{self.assigner_text_1.capitalize} D#{dice_value} value setted to none"
   end
 
   def assigners_validation(type_of_assign_validation)
     if type_of_assign_validation == 'cr'
-      assigner_1_validation = @@critic
-      assigner_2_validation = @@blunder
-      assigner_text_1_validation = 'critic'
-      assigner_text_2_validation = 'blunder'
+      self.assigner_1 = @@critic
+      self.assigner_2 = @@blunder
+      self.assigner_text_1 = 'critic'
+      self.assigner_text_2 = 'blunder'
     end
     if type_of_assign_validation == 'bl'
-      assigner_1_validation = @@blunder
-      assigner_2_validation = @@critic
-      assigner_text_1_validation = 'blunder'
-      assigner_text_2_validation = 'critic'
+      self.assigner_1 = @@blunder
+      self.assigner_2 = @@critic
+      self.assigner_text_1 = 'blunder'
+      self.assigner_text_2 = 'critic'
     end
-    [assigner_1_validation, assigner_2_validation, assigner_text_1_validation, assigner_text_2_validation]
   end
 
   def cr_bl_status(dice_type, type_of_assign)
